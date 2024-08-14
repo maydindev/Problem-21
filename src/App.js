bcd4d8e9affb4b62b883be0b9ec964ffb913f634
maydindev <maydin.dev@outlook.com>
2024-08-14 14:24:05 +0300

    Updated.

--- src/App.js
+++ src/App.js
@@ -1,54 +1,52 @@
-import { useEffect, useState } from 'react'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 // Bileşenin bağlantısı kaldırıldığında/unmounted uygulamamızın state'i güncellemesini durdurmak için useEffect'teki cleanup fonksiyonunu kullanın
 export default function App() {
-
-
   return (
-    <div className='flex justify-center flex-col items-center py-8'>
-      <h1 className='text-2xl font-bold pb-4'>🐭</h1>
+    <div className="flex justify-center flex-col items-center py-8">
+      <h1 className="text-2xl font-bold pb-4">🐭</h1>
       <BrowserRouter>
-        <nav className='flex justify-center max-w-sm p-4 pb-8 space-x-4'>
-          <Link className='underline' to='/'>
+        <nav className="flex justify-center max-w-sm p-4 pb-8 space-x-4">
+          <Link className="underline" to="/">
             Home
           </Link>
-          <Link className='underline' to='/about'>
+          <Link className="underline" to="/about">
             About
           </Link>
         </nav>
         <Routes>
-          <Route path='/' element={<MousePosition />} />
+          <Route path="/" element={<MousePosition />} />
           <Route
-            path='/about'
+            path="/about"
             element={<h1>Mouse&apos;unuzu takip edin!</h1>}
           />
         </Routes>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
 function MousePosition() {
-  const [position, setPosition] = useState({ x: 0, y: 0 })
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  function handleMove(e) {
+    setPosition({ x: e.clientX, y: e.clientY });
+    console.log("Updating state");
+  }
 
   useEffect(() => {
+    window.addEventListener("pointermove", handleMove);
     
-    function handleMove(e) {
-      setPosition({ x: e.clientX, y: e.clientY })
-      console.log('Updating state')
-    }
-    window.addEventListener('pointermove', handleMove)
 
     return () => {
-      window.removeEventListener("pointermove",handleMove)
-      console.log('Unmounted')
-    }
-  }
-, [])
+      window.removeEventListener("pointermove", handleMove);
+      console.log("Unmounted");
+    };
+  }, []);
 
   return (
-    <div className='space-y-5'>
+    <div className="space-y-5">
       <div>
         X position: <strong>{position.x.toFixed(2)}</strong>
       </div>
@@ -56,5 +54,5 @@ function MousePosition() {
         Y position: <strong>{position.y.toFixed(2)}</strong>
       </div>
     </div>
-  )
+  );
 }
